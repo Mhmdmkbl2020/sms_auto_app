461db491a86b85e20de65130b6b02305e74c6ee8
Mhmdmkbl2020 <133782007+Mhmdmkbl2020@users.noreply.github.com>
2025-04-26 17:18:33 +0300

    Create SMSSender.java

---
+++ SMSSender.java
@@ -0,0 +1,8 @@
+public class SmsSender {
+
+    public static void sendSms(String phoneNumber, String message) {
+        System.out.println("Sending SMS to: " + phoneNumber);
+        System.out.println("Message: " + message);
+        // هنا يمكن إضافة كود إرسال الرسائل باستخدام مكتبة GSM
+    }
+}
